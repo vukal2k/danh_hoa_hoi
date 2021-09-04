8b9f4a9a5ec2b50ae8f30d4eb4f36ee1aadc7403
nguyen.tuananh <nguyen.tuananh@petronas.com>
2021-09-04 22:11:38 +0700

    aa

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 var crypto = require("crypto");
 
-const token = 'CountryCode=VN; userFromEEA=false; __gads=ID=8c2ddb028ad9cfe2:T=1629900959:S=ALNI_MYWvUBDVwRrGoaGQCDuiyvKIL2gqQ; garticio=s%3A59530c12-7238-400e-8681-7279f46bb351.%2FgWeWX8PienhfAj2fNSKCQdrZFiRYe1sUJUTWLbGWEE; _gid=GA1.2.139362625.1630156359; __cf_bm=e811201f856bad680e454c3e011251169a0e496c-1630163381-1800-Afzdj6Ix2CbxgV9nvdL+YmYMV/7hntpCQyrUk/6vka/0P9UfjB9wbz+UlPHRlWdyOPxgpEue2wTJaBZC/l5VhyQ=; _gat_gtag_UA_3906902_31=1; _ga=GA1.2.230272424.1629900957; session_depth=gartic.io%3D13%7C772647598%3D13; _ga_VR1WBQ9P5N=GS1.1.1630159640.4.1.1630163426.0';
+const token = 'garticio=s%3Abd83eab5-751e-45df-8c8b-34f14ad109a5.KEGYCFMB%2FOlImPw32LbA68GCgKCd5p8uPuX%2BlW%2BDLd0; __cf_bm=EfbdWqGm5J52Oko0yZP84PtgrM1T29vJvzIIKRi5eh0-1630766542-0-AbKUcCbNozWN63A7DdLxlRO+rlKmiiBS4I2/YpmW0VwQDo8V5pChW+P1KTTIaWzgGqMicBmOz4PRpXzkICfB9NI=; pg_analytics=disabled; pg_mm2_cookie_a=21f21584-270a-45bd-967c-a71b882c100c; pg_session_id=674c1cf0-7052-4897-b381-dbb0a14ec581; pg_tc=sample; pg_pl=13; pg_quick_check=true; pg_ua=Mozilla/5.0 (Windows NT 10.0 Win64 x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36; pg_beacon=1; _gid=GA1.2.1305567673.1630766544; pg_geo={"country":"VN","region":"HN","ip":"123.24.6.101"}; pg_custom_timeout=; pg_ip=123.24.6.101; pg_bot_reason=lnb; pg_bot_percent=0; pg_bot_model=1; pg_height=1080; pg_width=1920; pg_aheight=1040; pg_awidth=1920; pg_last_unload=8943; pg_pv_time_1=8943; pg_mouse_move=214; pg_keypress=0; pg_click=2; pg_touch_move=0; pg_scroll=0; pg_touch_start=0; pg_session_depth=2; pg_latency_before_tc=96; pg_canonical_session=fa08fdd6562ea53a4a9e01a962cc2a01; _ga=GA1.2.470788309.1630766544; __gads=ID=183721a98ce530d3:T=1630766544:S=ALNI_MaNqVp56vesSw41ro7tGXKGYyxyvQ; pg_tc_response_time=2078; _ga_VR1WBQ9P5N=GS1.1.1630766543.1.1.1630766838.0; pg_pv_time_2=70075';
 
 
-const getListRemoveWord = async () => {
+const getListRemoveWord = async (listRemove) => {
+    if(listRemove){
+        return listRemove;
+    }
     let result = await axios.get('https://gartic.io/req/subject?subject=30&language=13', {
         headers:{
             cookie: token
@@ -63,9 +66,15 @@ const updateData = async(added, removed)=>{
 }
 
 const main = async()=>{
-    const removed = await getListRemoveWord();
+    //1. Sync list
+    let removed = await getListRemoveWord();
     let added = await getListTiengVietWord();
-    const updateStatus = await updateData(added, removed);
+    let updateStatus = await updateData(added, removed);
+
+    //2. Remove list unused
+    removed = ['ồ','sự làm rỗng','sự sưng lên','tắt','gạch sống','xà neo','trầm','ép','keo kiệt'];
+    added = [];
+    updateStatus = await updateData(added, removed);
 
     console.log(updateStatus)
 };
